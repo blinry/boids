b74a4a447b17a9c05b63e6998b7e349fd4011dfc
Sebastian Morr <sebastian@morr.cc>
2020-09-09 22:07:02 +0200

    Nice swarm

--- main.gd
+++ main.gd
@@ -1,11 +1,17 @@
 extends Node2D
 
+var last_pos
+
 func _ready():
 	pass
 		
 func _process(delta):
+	var m = get_global_mouse_position()
 	if Input.is_action_pressed("spawn"):
 		var pod = preload("res://boid.tscn").instance()
-		pod.position = get_global_mouse_position()
-		pod.velocity = Vector2(rand_range(-300, 300), rand_range(-300, 300))
+		var width = 50
+		pod.position = m + Vector2(rand_range(-width, width), rand_range(-width, width))
+		pod.velocity = rand_range(200, 300)*(m - last_pos).normalized()
+		#pod.velocity = Vector2(rand_range(-300, 300), rand_range(-300, 300))
 		add_child(pod)
+	last_pos = m
