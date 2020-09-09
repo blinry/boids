fc4d198b2449ecba4465166917b7b71972969737
Sebastian Morr <sebastian@morr.cc>
2020-09-09 20:41:42 +0200

    Avoidance

--- boid.gd
+++ boid.gd
@@ -3,16 +3,39 @@ extends Node2D
 var velocity = Vector2(0, 100)
 
 func _process(delta):
+	if get_parent().average:
+		velocity += cohesion()*10
+	velocity += separation()
+	
+	velocity += edge_avoid()*500
+	
+	velocity *= 0.99
+	
 	position += delta*velocity
 	rotation = velocity.angle()
-	
-	var screen_size = get_viewport_rect().size
+		
+func cohesion():
+	var change = (get_parent().average-position).normalized()
+	#print(change)
+	return change
 
-	if position.x < 0:
-		position.x = screen_size.x
-	if position.y < 0:
-		position.y = screen_size.y
-	if position.x > screen_size.x:
-		position.x = 0
-	if position.y > screen_size.y:
-		position.y = 0
+func separation():
+	var change = Vector2.ZERO
+	for boid in get_tree().get_nodes_in_group("boids"):
+		var diff = (boid.position - position)
+		if diff.length() < 100:
+			change -= diff
+	return change
+
+func edge_avoid():
+	var screen_size = get_viewport_rect().size
+	var change = Vector2.ZERO
+	if position.x < 100:
+		change += Vector2(1, 0)
+	if position.y < 100:
+		change += Vector2(0, 1)
+	if position.x > screen_size.x-100:
+		change += Vector2(-1, 0)
+	if position.y > screen_size.y-100:
+		change += Vector2(0, -1)
+	return change
--- main.gd
+++ main.gd
@@ -1,8 +1,9 @@
 extends Node2D
 
+var average
+
 func _ready():
 	pass
-
 		
 func _process(delta):
 	if Input.is_action_pressed("spawn"):
@@ -10,3 +11,9 @@ func _process(delta):
 		pod.position = get_global_mouse_position()
 		pod.velocity = Vector2(rand_range(-300, 300), rand_range(-300, 300))
 		add_child(pod)
+
+	average = Vector2.ZERO
+	var boids = get_tree().get_nodes_in_group("boids")
+	for boid in boids:
+		average += boid.position
+	average = average/boids.size()
